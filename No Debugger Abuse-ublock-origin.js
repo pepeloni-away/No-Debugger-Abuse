55cf38873e7a63489bd8e4071382226bfe67c100
pploni <80548307+pepeloni-away@users.noreply.github.com>
2022-12-25 00:16:06 +0200

    Update No Debugger Abuse-ublock-origin.js

--- No Debugger Abuse-ublock-origin.js
+++ No Debugger Abuse-ublock-origin.js
@@ -10,32 +10,32 @@
 /// no-debugger-abuse.js
 /// alias nda.js
 (function(){
-	console.log('{{1}}')
-	const logs = true
-	const details = true
-	const here = location.href.replace(/^http(s)?:\/\//, '') > 75 ?
-      location.href.replace(/^http(s)?:\/\//, '').substring(0,75) + '...' : location.href.replace(/^http(s)?:\/\//, '')
-	const l = (...args) => (console.log?.name == 'log' ? console.log : false || console.log?.name == 'info' ? console.log : false || console.debug)(...args)
-let lastCall
+	const logs = true;
+const details = true;
+const here = location.href.replace(/^http(s)?:\/\//, '') > 75 ?
+      location.href.replace(/^http(s)?:\/\//, '').substring(0,75) + '...' : location.href.replace(/^http(s)?:\/\//, '');
+const l = (...args) => (console.log?.name == 'log' ? console.log : false || console.log?.name == 'info' ? console.log : false || console.debug)('tmp', ...args);
+let lastCall;
 self.Function.prototype.constructor = new Proxy(self.Function.prototype.constructor, {
    apply: function(target, thisArg, args) {
-       let fnContent, callerContent, date, diff
+       let fnContent, callerContent, date, diff;
        try {
-           fnContent = args[0]
-           callerContent = thisArg?.toString()
-           date = new Date()
-           lastCall ? diff = date.getTime() - lastCall.getTime() : diff = 0
+           fnContent = args[0];
+           callerContent = thisArg?.toString();
+           date = new Date();
+           lastCall ? diff = date.getTime() - lastCall.getTime() : diff = 0;
            logs ? details ? l(`debugger call on ${here}`, diff, `ms since last call\ncalled by`, thisArg, `content:\n${callerContent}`) :
-           l(`debugger call on ${here}`, diff, `ms since last call`) : null
-           lastCall = date
+           l(`debugger call on ${here}`, diff, `ms since last call`) : null;
+           lastCall = date;
        } catch (err) {
-           if (logs) l(err, '\ndebugger logger failed\narguments:', arguments)
+           if (logs) l(err, '\ndebugger logger failed\narguments:', arguments);
        }
-       if ('debugger' === fnContent) return
-       return target.apply(thisArg, args)
+       if ('debugger' === fnContent) return;
+       return target.apply(thisArg, args);
    }
 })
-self.Function = self.Function.constructor // some libs check if sth is a function by comparing its constructor with Function
+self.Function = self.Function.constructor; // some libs check if sth is a function by comparing its constructor with Function
+
 
 })();
 
